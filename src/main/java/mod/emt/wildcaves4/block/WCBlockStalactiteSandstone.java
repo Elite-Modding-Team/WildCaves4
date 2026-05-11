627764b1b9c754796f10ad4ae45b980d65f179ba
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-05-11 08:09:08 +0200

    Fix stalactites supporting torches etc.
    Closes #11

--- src/main/java/mod/emt/wildcaves4/block/WCBlockStalactiteSandstone.java
+++ src/main/java/mod/emt/wildcaves4/block/WCBlockStalactiteSandstone.java
@@ -1,12 +1,12 @@
 package mod.emt.wildcaves4.block;
 
-import mod.emt.wildcaves4.WildCaves;
 import mod.emt.wildcaves4.config.WCConfig;
 import mod.emt.wildcaves4.init.WCBlocks;
 import mod.emt.wildcaves4.util.WCUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -17,6 +17,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -204,6 +205,11 @@ public class WCBlockStalactiteSandstone extends Block {
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess par1IBlockAccess, BlockPos pos) {
         return WCUtils.getBox(getMetaFromState(state));
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
     }
 
     @Override
